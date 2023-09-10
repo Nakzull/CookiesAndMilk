0f44d22aa7602bb46e229c2a0b119572880a1bd7
Nakzull <andr25f4@zbc.dk>
2023-09-10 19:46:00 +0200

    Add project files.

---
+++ CookiesAndMilk/Controllers/CookieController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CookiesAndMilk.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CookieController : ControllerBase
+    {
+        [HttpGet("FavoriteMilkshake")]
+        public string Get(string favoriteMilkshake)
+        {
+            CookieOptions cookieOptions = new CookieOptions();
+            cookieOptions.Expires = DateTimeOffset.Now.AddMinutes(20);
+            Response.Cookies.Append("favoriteMilkshake", favoriteMilkshake, cookieOptions);
+            return favoriteMilkshake;
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public string GetFromCookie()
+        {
+            if (Request.Cookies["favoriteMilkshake"] != null)
+            {
+                return Request.Cookies["favoriteMilkshake"];
+            }
+            else
+                return "unknown";
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public void DeleteCookie()
+        {
+            if (Request.Cookies["favoriteMilkshake"] != null)
+            {
+                Response.Cookies.Delete("favoriteMilkshake");
+            }
+        }
+    }
+}
---
+++ CookiesAndMilk/Controllers/ShoppingCartContoller.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookiesAndMilk.Controllers
+{
+    [Route("shopping/[controller]")]
+    [ApiController]
+    public class ShoppingCartContoller : Controller
+    {
+        [HttpPost("AddProduct")]
+        public ActionResult Add([FromBody] Product product)
+        {
+            if (product.Price >= 0 && product.Price < 10)
+            {
+                List<Product> cart = HttpContext.Session.GetObjectFromJson<List<Product>>("Cart") ?? new List<Product>();
+                cart.Add(product);
+                HttpContext.Session.SetObjectAsJson("Cart", cart);
+                return Ok();
+            }
+            return BadRequest("You need to enter a price in numbers");
+        }
+
+        [HttpGet("GetCart")]
+        public List<Product> Get()
+        {
+            return HttpContext.Session.GetObjectFromJson<List<Product>>("Cart");
+        }
+
+        [HttpPost("RemoveProduct")]
+        public void SetSession([FromBody] Product product)
+        {
+            List<Product> cart = HttpContext.Session.GetObjectFromJson<List<Product>>("Cart");
+            if (cart != null)
+            {
+                for (int i = 0; i < cart.Count; i++)
+                {
+                    if (cart[i].Name == product.Name && cart[i].Price == product.Price)
+                    {
+                        cart.RemoveAt(i);
+                    }
+                    HttpContext.Session.SetObjectAsJson("Cart", cart);
+                }
+            }
+        }
+    }
+}
+
